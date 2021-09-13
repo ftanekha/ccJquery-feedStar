8ce4fcab0c16395de6c6561fdcacc152d76d2256
Farai Tanekha <30661410+codemusltd@users.noreply.github.com>
2021-09-13 17:06:06 +0100

    add hover functionality to navigation menu and buttons

--- main.js
+++ main.js
@@ -1,5 +1,14 @@
 $(document).ready(
     () => {
-        
+        //make a navigation menu appear when you hover over menu, 
+        //and make it disappear when you navigate away from the navigation menu
+        $('.menu').on('mouseover', () => $('.nav-menu').slideDown('slow'))
+        //hide the .nav-menu element when a user’s mouse leaves the .nav-menu element
+        $('.nav-menu').on('mouseleave', () => $('.nav-menu').slideUp('slow'))
+
+        //add/remove the .btn-hover class to .btn elements when a user mouses over/ leaves a .btn element
+        $('.btn')
+        .on('mouseover', ({currentTarget}) => $(currentTarget).addClass('btn-hover'))
+        .on('mouseleave', ({currentTarget}) => $(currentTarget).removeClass('btn-hover'))
     }
 )
