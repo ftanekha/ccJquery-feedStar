91a2679d8bf543bc6fdaa9763cb1887fdd1cf43b
Farai Tanekha <30661410+codemusltd@users.noreply.github.com>
2021-09-13 17:24:44 +0100

    limit user's post to 140 characters final commit

--- main.js
+++ main.js
@@ -10,5 +10,20 @@ $(document).ready(
         $('.btn')
         .on('mouseover', ({currentTarget}) => $(currentTarget).addClass('btn-hover'))
         .on('mouseleave', ({currentTarget}) => $(currentTarget).removeClass('btn-hover'))
+
+        //display the remaining number of characters that a user can enter into their comment box
+        $('.postText')
+        .on('keyup', ({currentTarget}) => {
+            const post = $(currentTarget).val().length,
+            remaining = 140 - post
+
+            $('.characters').html(remaining)
+
+            //make the '.wordcount' message turn red if the user runs out of characters
+            remaining <=0 && $('.wordcount').addClass('red') || $('.wordcount').removeClass('red')
+        })
+        .on('focus', () => {
+            //This will cause the <textarea> to expect typed text as soon as the page loads
+        })
     }
 )
